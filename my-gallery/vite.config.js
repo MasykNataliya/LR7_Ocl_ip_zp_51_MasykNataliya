5345d641f60bfe6a1d258b7cdd59f16195ff8f87
Nata <masyknataliya@gmail.com>
2026-06-08 03:44:17 +0300

    vc corr

--- my-gallery/vite.config.js
+++ my-gallery/vite.config.js
@@ -2,13 +2,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/LR7_Ocl_ip_zp_51_MasykNataliya/",
-  resolve: {
-    alias: {
-      simplelightbox: "simplelightbox/dist/simple-lightbox.esm.js",
-    },
-  },
-  define: {
-    global: "window",
+  server: {
+    open: true,
   },
 });
-import { defineConfig } from "vite";
